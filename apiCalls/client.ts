4aa92265c682d254b1bd9151f0cd98adf54f4494
IvanRomero03 <i.wells.ar@gmail.com>
2022-08-22 20:06:31 -0500

    client2

--- apiCalls/client.ts
+++ apiCalls/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://lista-compras-next.vercel.app/api",
+  baseURL: "https://lista-compras-next.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
